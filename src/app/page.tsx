e2fc832d8895d990ea35bde5246ae78bce4c1afa
Aris Hadrian <ashenarmor1013@gmail.com>
2025-06-21 04:11:59 +0600

    Implement Get In Touch section with contact form and info cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { GetInTouch } from "@/components/common/GetInTouch";
 import HeroSection from "@/components/common/HeroSection";
 
 export default function Home() {
-  return <HeroSection />;
+  return (
+    <main>
+      <HeroSection />
+      <GetInTouch />
+    </main>
+  );
 }
