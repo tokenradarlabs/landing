832ee89bd696d7d436a99f7222dcc3bedbfcaa19
Ethan Wallace <alu.pesto.en@gmail.com>
2025-06-24 07:34:35 +0600

    Sorted landing page placements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
+      <AboutSection />
       <PlatformFeatures />
       <GetInTouch />
-      <AboutSection />
     </main>
   );
 }
