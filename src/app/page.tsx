ea0393b9b9da42c243beb82665d82c745e3f370f
Marko Piers <markopiers8@gmail.com>
2025-07-02 13:46:51 -0600

    Add Developer Resources and Support Links section to homepage with Discord support link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/common/HeroSection";
 import PlatformFeatures from "@/components/common/PlatformFeatures";
 import AboutSection from "@/components/common/AboutSection";
 import FAQSection from "@/components/common/FAQSection";
+import ComingSoon from "@/components/common/ComingSoon";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <PlatformFeatures />
+      <ComingSoon />
       <FAQSection />
       <GetInTouch />
     </main>
