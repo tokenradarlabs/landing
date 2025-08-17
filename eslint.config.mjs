8f621f1cba624dbe5e52ee2217be23544fdf4d0a
Tasin Williamson <tasinwilliamson@gmail.com>
2025-08-17 01:01:27 -0700

    Added esline config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,44 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended"
+  ),
+  {
+    rules: {
+      // Possible Errors
+      'no-console': 'warn',
+      'no-debugger': 'error',
+
+      // Best Practices
+      'eqeqeq': ['error', 'always'],
+      'curly': 'error',
+
+      // Stylistic
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'double', { avoidEscape: true }],
+      'comma-dangle': ['error', 'always-multiline'],
+      'object-curly-spacing': ['error', 'always'],
+
+      // React
+      'react/react-in-jsx-scope': 'off', // Not needed for Next.js
+      'react/prop-types': 'off', // Using TypeScript
+
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      // Accessibility
+      'jsx-a11y/anchor-is-valid': 'warn',
+    },
+  },
 ];
 
 export default eslintConfig;
